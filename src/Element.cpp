8932480c2ca7b7044b6587c7463693ffc3601eb1
Jonas Toth <jonas.toth@protonmail.com>
2015-12-01 21:33:50 +0100

    Initial commit for prototyping the structoeditor

---
+++ src/Element.cpp
@@ -0,0 +1,8 @@
+#include "Element.h"
+
+Element::Element() :
+	__before{ nullptr },
+	__after{ nullptr }
+{
+
+}
---
+++ src/Element.h
@@ -0,0 +1,79 @@
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+#include <memory>
+
+
+/**
+* @brief Abstract class for every element in a structogram
+*
+* Gives basic functionality for every element within a structogram, but does not hold
+* the structure for a Structogram. 
+* The general idea is a linked list, with the links connecting the elements.
+* Every structogram must contain such nodes and save them in an extra data structure.
+*/
+class Element 
+{
+public:
+	/**
+	* @brief default construction, the root of everything, will not contain any data
+	*/
+	Element ();
+
+	/**
+	* @brief construct element with existing connections, takes ownership of the pointers
+	*
+	* @param before element before, will take ownership of it
+	* @param after element after, will take ownership of it
+	*/
+	Element (Element* before, Element* after = nullptr);
+
+	/**
+	* @brief construct element with connection to element before, rest is nullptr
+	*
+	* @param before element before, no ownership
+	*/
+	Element (std::shared_ptr<Element>& before);
+
+	/**
+	* @brief construct element with connections to before and after
+	*
+	* @param before element before, no ownership
+	* @param after element after, no ownership
+	*/
+	Element (std::shared_ptr<Element>& before, std::shared_ptr<Element>& after);
+
+
+	/**
+	* @brief get the next element in the evaluation chain
+	*
+	* @return shared_ptr to next element
+	*/
+	virtual std::shared_ptr<Element>& getNext();
+
+	/**
+	* @brief Does nothing, since shared_ptr will do all the deleting and reference counting
+	*/
+	virtual ~Element () {}
+
+	/**
+	* @brief get element before the element in the structogram chain
+	*
+	* @return reference to shared ptr for mutliple use of the object
+	*/
+	const std::shared_ptr<Element>& getBefore() const { return __before; }
+
+	/**
+	* @brief get element after the element in the structogram chain
+	*
+	* @return reference to shared ptr for multiple use of the element
+	*/
+	const std::shared_ptr<Element>& getAfter() const  { return __after; }
+
+private:
+	std::shared_ptr<Element>	__before; 	/**< The element before the structogram */
+	std::shared_ptr<Element>	__after;	/**< The element after the structogram */
+};
+
+
+#endif /* end of include guard: ELEMENT_H */
